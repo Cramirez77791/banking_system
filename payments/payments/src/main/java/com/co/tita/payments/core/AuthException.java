19d316cfecb59ce996095a5f4833fb08d0b7e3b0
azulm95 <azulm95@gmail.com>
2022-11-25 03:06:22 -0500

    configuracion de tomcat terminada

--- payments/payments/src/main/java/com/co/tita/payments/core/AuthException.java
+++
@@ -1,9 +0,0 @@
-package com.co.tita.payments.core;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class AuthException extends AuthenticationException {
-    public AuthException(String msg) {
-        super(msg);
-    }
-}
